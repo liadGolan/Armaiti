b28182b3a8da167469a5b8968d9b69c39c725bbe
Liad Golan <liad.golan.736@my.csun.edu>
2019-01-31 09:56:00 -0800

    update fractal music to be based on same file export

--- FractalMusic.py
+++ FractalMusic.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 
-import music21
+from music21 import *
 
 
 class Harmony:
     def __init__(self, start, steps):
-        self.one_chord = music21.chord.Chord(['F2', 'F3', 'A3', 'C4'], quarterLength=2)
-        self.two_chord = music21.chord.Chord(['G2', 'G3', 'B-3', 'D4'], quarterLength=2)
-        self.three_chord = music21.chord.Chord(['A2', 'A3', 'C4', 'E4'], quarterLength=2)
-        self.four_chord = music21.chord.Chord(['B-2', 'B-3', 'D4', 'F4'], quarterLength=2)
-        self.five_chord = music21.chord.Chord(['C3', 'C4', 'E4', 'G4'], quarterLength=2)
-        self.six_chord = music21.chord.Chord(['D3', 'D4', 'F4', 'A4'], quarterLength=2)
-        self.seven_chord = music21.chord.Chord(['E3', 'E4', 'G4', 'B-4'], quarterLength=2)
+        self.one_chord = chord.Chord(['F2', 'F3', 'A3', 'C4'], quarterLength=2)
+        self.two_chord = chord.Chord(['G2', 'G3', 'B-3', 'D4'], quarterLength=2)
+        self.three_chord = chord.Chord(['A2', 'A3', 'C4', 'E4'], quarterLength=2)
+        self.four_chord = chord.Chord(['B-2', 'B-3', 'D4', 'F4'], quarterLength=2)
+        self.five_chord = chord.Chord(['C3', 'C4', 'E4', 'G4'], quarterLength=2)
+        self.six_chord = chord.Chord(['D3', 'D4', 'F4', 'A4'], quarterLength=2)
+        self.seven_chord = chord.Chord(['E3', 'E4', 'G4', 'B-4'], quarterLength=2)
 
         self.one_chords = []
         self.two_chords = []
@@ -68,22 +68,22 @@ class Harmony:
 
             self.pos += 2
 
-        stream = music21.stream.Stream()
-        stream.repeatInsert(self.one_chord, self.one_chords)
-        stream.repeatInsert(self.two_chord, self.two_chords)
-        stream.repeatInsert(self.three_chord, self.three_chords)
-        stream.repeatInsert(self.four_chord, self.four_chords)
-        stream.repeatInsert(self.five_chord, self.five_chords)
-        stream.repeatInsert(self.six_chord, self.six_chords)
-        stream.repeatInsert(self.seven_chord, self.seven_chords)
-        return stream
+        stram = stream.Stream()
+        stram.repeatInsert(self.one_chord, self.one_chords)
+        stram.repeatInsert(self.two_chord, self.two_chords)
+        stram.repeatInsert(self.three_chord, self.three_chords)
+        stram.repeatInsert(self.four_chord, self.four_chords)
+        stram.repeatInsert(self.five_chord, self.five_chords)
+        stram.repeatInsert(self.six_chord, self.six_chords)
+        stram.repeatInsert(self.seven_chord, self.seven_chords)
+        return stram
 
 
 def main():
-    harmony = Harmony('one', 3)
-    harmony_stream = harmony.get_final_build()
-    harmony_stream.insert(0, music21.tempo.MetronomeMark(number=150))
-    harmony_stream.show()
+    hamony = Harmony('one', 4)
+    hamony_stream = hamony.get_final_build()
+    hamony_stream.insert(0, tempo.MetronomeMark(number=150))
+    hamony_stream.show()
 
 
 if __name__ == '__main__':
---
+++ helpers/MelodyAdjustor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+from music21 import *
+
+def melody_adjustor(current_chord, current_note):
